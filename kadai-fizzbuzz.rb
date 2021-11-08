521f176d3107d83a6de0ea9dfd8b69115e20afb9
yota <heyetech01@gmail.com>
2021-11-08 12:16:12 +0000

    修正１

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,17 +1,20 @@
 def fizzbuzz(num)
   
   if num % 3 == 0 && num % 5 == 0
-    puts "FizzBuzz"
+   "FizzBuzz"
   elsif  num % 5 == 0
-    puts "Buzz"
+   "Buzz"
   elsif num % 3 == 0 
-    puts "Fizz"
+   "Fizz"
   else
-    puts num.to_s
+   num.to_s
   end
 
 end
 
-(1..num_max = 100) .each do |num|
+num_max = 100
+
+(1 .. num_max) .each do |num|
   puts fizzbuzz(num)
+
 end
